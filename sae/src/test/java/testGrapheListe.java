62b45a6ea49035a69ac95c76a16b05086b1bcec9
Baloo54 <gabriel.comte2006@gmail.com>
2024-05-30 10:45:12 +0200

    save change

--- sae/src/test/java/testGrapheListe.java
+++ sae/src/test/java/testGrapheListe.java
@@ -1,3 +1,27 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import algorithme.*;
 public class testGrapheListe {
-    
+    // test création d'un graphe simple
+    @Test
+    public void testGrapheListe() {
+        GrapheListe graphe = new GrapheListe();
+        graphe.ajouterArc("A", "B", 12);
+        graphe.ajouterArc("A", "D", 87);
+        graphe.ajouterArc("B", "E", 11);
+        graphe.ajouterArc("E", "D", 43);
+        graphe.ajouterArc("D", "B", 23);
+        graphe.ajouterArc("D", "C", 10);
+        graphe.ajouterArc("C", "A", 19);
+        System.out.println(graphe);
+    }
+    // lancement des tests
+    public static void main(String[] args) {
+        org.junit.jupiter.api.Assertions.assertDoesNotThrow(() -> {
+            testGrapheListe test = new testGrapheListe();
+            test.testGrapheListe();
+        });
+    }
 }
+
+
